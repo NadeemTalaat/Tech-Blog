5f60d2ff6cd797ba876d8d15fb55d830c9c2a17a
NadeemTalaat <nadeem.talaat@gmail.com>
2023-06-11 16:25:29 -0400

    Define get route for home-routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const { Post } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll();
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("homepage", {
+      posts,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
